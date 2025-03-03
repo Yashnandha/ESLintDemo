85fc7df65168cf8b618e75f0521c8d0528b90823
Yash Nandha <yash.vhits@gmail.com>
2025-03-03 15:23:18 +0530

    init to win

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import callstackConfig from '@callstack/eslint-config/react-native.flat.js';
+
+export default [
+  {
+    ignores: [
+      // ignored files go here
+      node_modules,
+      dist,
+      build,
+    ],
+  },
+  ...callstackConfig,
+  {
+    rules: {
+      // your custom rules
+      'no-unused-vars': 'error',
+    },
+  },
+];
