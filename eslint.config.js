04d165c529921b5561ebb274a2736501b17d420f
Yash Nandha <yash.vhits@gmail.com>
2025-03-03 14:59:52 +0530

    init to win

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
 import pluginJs from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
-import prettier from 'eslint-plugin-prettier';
+import prettierPlugin from 'eslint-plugin-prettier'; // Use 'prettierPlugin' instead
 import globals from 'globals';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     languageOptions: {
       globals: {
-        ...globals.node, // Add Node.js globals like `process`
+        ...globals.node,
       },
     },
+    ignores: ['node_modules/', 'dist/', 'build/', 'coverage/', '.env'],
   },
   pluginJs.configs.recommended,
   {
     plugins: {
-      prettier,
+      prettier: prettierPlugin, // Correctly reference the plugin
     },
     rules: {
       ...prettierConfig.rules,
