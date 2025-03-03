79bfdc16bd9966e5488204b003fe5e112b3fe4b2
Yash Nandha <yash.vhits@gmail.com>
2025-03-03 16:52:09 +0530

    Add Colors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,9 @@ export default [
       'react-native/no-inline-styles': 'error',
       'react-native/no-color-literals': 'error',
       'react-native/no-raw-text': 'error',
+      'react-native/no-single-element-style-arrays': 'error',
+      'react-native/sort-styles': 'error',
+      'react-native/split-platform-components': 'error',
     },
   },
 ];
