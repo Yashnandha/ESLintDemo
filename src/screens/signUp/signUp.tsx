4f05a6435341b18666aee5f89e9b9c43563ed3a4
Yash Nandha <yash.vhits@gmail.com>
2025-03-03 17:31:53 +0530

    Add files

--- src/screens/signUp/signUp.tsx
+++ src/screens/signUp/signUp.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 
 const signUp = () => {
-  return (
-    <View>
-    </View>
-  );
+  return <View></View>;
 };
 
 export default signUp;
