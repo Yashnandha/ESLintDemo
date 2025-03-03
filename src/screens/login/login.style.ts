79e8731192725cf82653c94b640ecf25ce0536bb
Yash Nandha <yash.vhits@gmail.com>
2025-03-03 17:56:56 +0530

    Add package

--- src/screens/login/login.style.ts
+++ src/screens/login/login.style.ts
@@ -10,6 +10,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    aspectRatio: '16:9',
   },
 });
 export default styles;
--- src/screens/signUp/signUp.tsx
+++ src/screens/signUp/signUp.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import colors from '../../theme/colors';
 
 const signUp = () => {
-  return <View></View>;
+  return <View style={styles.container}></View>;
 };
 
 export default signUp;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
